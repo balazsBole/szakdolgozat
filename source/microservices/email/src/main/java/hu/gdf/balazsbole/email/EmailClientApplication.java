607c35a02587c025bae0828c2c9985393af07f86
Balázs <bolebalazs@gmail.com>
2020-10-05 22:00:04 +0200

    EmailClientApplication created

---
+++ source/microservices/email/src/main/java/hu/gdf/balazsbole/email/EmailClientApplication.java
@@ -0,0 +1,13 @@
+package hu.gdf.balazsbole.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailClientApplication.class, args);
+	}
+
+}
---
+++ source/microservices/email/src/test/java/hu/gdf/balazsbole/email/EmailClientApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gdf.balazsbole.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
