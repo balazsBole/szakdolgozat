90cfe71ede3693a2a264dc87b0db01cd47192686
Balázs <bolebalazs@gmail.com>
2020-11-09 00:41:01 +0100

    kafka brokers and grafana metrics added

--- source/infrastructure/start.sh
+++ source/infrastructure/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker-compose -f docker-compose.yaml up -d --scale helpdesk-backend=3 --scale email-client-generic=3 --scale email-client-travel=2
+docker-compose -f docker-compose.yaml up -d \
+  --scale helpdesk-backend=3 \
+  --scale email-client-generic=3 \
+  --scale email-client-travel=2 \
+  --scale kafka=3
 
-docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} helpdesk' infrastructure_helpdesk_1  | sudo tee -a /etc/hosts
+docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} helpdesk' infrastructure_helpdesk_1 | sudo tee -a /etc/hosts
